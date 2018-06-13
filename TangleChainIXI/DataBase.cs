435d062ce6f3bd46fd130bf60d564c5a42cacc2b
Kelvin <test@test.com>
2018-06-13 16:40:57 +0200

    DB lock fixed

--- TangleChainIXI/DataBase.cs
+++ TangleChainIXI/DataBase.cs
@@ -20,28 +20,29 @@ namespace TangleChainIXI {
             //first we create file structure
             if (!Directory.Exists($@"{path}{name}\")) {
                 Directory.CreateDirectory($@"{path}{name}\");
+
+                string sql =
+                    "CREATE TABLE IF NOT EXISTS Block (Height INT PRIMARY KEY, Nonce INT NOT NULL, Time LONG NOT NULL, Hash CHAR(20) NOT NULL, " +
+                    "NextAddress CHAR(81) NOT NULL, Owner CHAR(81) NOT NULL, SendTo CHAR(81) NOT NULL);";
+
+                string sql2 =
+                    "CREATE TABLE IF NOT EXISTS Transactions (ID INTEGER PRIMARY KEY AUTOINCREMENT, Hash CHAR(81), Time LONG, _From CHAR(81), Signature CHAR(81)," +
+                    "Mode INT,BlockID INT NOT NULL ,FOREIGN KEY(BlockID) REFERENCES Block(Height) ON DELETE CASCADE);";
+
+                string sql3 =
+                    "CREATE TABLE IF NOT EXISTS Data (ID INTEGER PRIMARY KEY AUTOINCREMENT, _ArrayIndex INT NOT NULL, " +
+                    "Data CHAR, TransID ,FOREIGN KEY (TransID) REFERENCES Transactions(ID) ON DELETE CASCADE);";
+
+                string sql4 =
+                    "CREATE TABLE IF NOT EXISTS Output (ID INTEGER PRIMARY KEY AUTOINCREMENT, _Values INT NOT NULL,_ArrayIndex INT NOT NULL, " +
+                    "Receiver CHAR, TransID ID NOT NULL,FOREIGN KEY(TransID) REFERENCES Transactions(ID) ON DELETE CASCADE);";
+
+                NoQuerySQL(sql);
+                NoQuerySQL(sql2);
+                NoQuerySQL(sql3);
+                NoQuerySQL(sql4);
+
             }
-
-            Db = new SQLiteConnection($@"Data Source={path}{name}\chain.db; Version=3;");
-            Db.Open();
-
-            
-            string sql = "CREATE TABLE IF NOT EXISTS Block (Height INT PRIMARY KEY, Nonce INT NOT NULL, Time LONG NOT NULL, Hash CHAR(20) NOT NULL, " +
-                         "NextAddress CHAR(81) NOT NULL, Owner CHAR(81) NOT NULL, SendTo CHAR(81) NOT NULL);";
-
-            string sql2 = "CREATE TABLE IF NOT EXISTS Transactions (ID INTEGER PRIMARY KEY AUTOINCREMENT, Hash CHAR(81), Time LONG, _From CHAR(81), Signature CHAR(81)," +
-                          "Mode INT,BlockID INT NOT NULL ,FOREIGN KEY(BlockID) REFERENCES Block(Height) ON DELETE CASCADE);";
-
-            string sql3 = "CREATE TABLE IF NOT EXISTS Data (ID INTEGER PRIMARY KEY AUTOINCREMENT, _ArrayIndex INT NOT NULL, " +
-                          "Data CHAR, TransID ,FOREIGN KEY (TransID) REFERENCES Transactions(ID) ON DELETE CASCADE);";
-
-            string sql4 = "CREATE TABLE IF NOT EXISTS Output (ID INTEGER PRIMARY KEY AUTOINCREMENT, _Values INT NOT NULL,_ArrayIndex INT NOT NULL, " +
-                          "Receiver CHAR, TransID ID NOT NULL,FOREIGN KEY(TransID) REFERENCES Transactions(ID) ON DELETE CASCADE);";
-
-            NoQuerySQL(sql);
-            NoQuerySQL(sql2);
-            NoQuerySQL(sql3);
-            NoQuerySQL(sql4);
 
         }
 
@@ -65,7 +66,7 @@ namespace TangleChainIXI {
 
                 if (transList != null)
                     AddTransaction(transList, block.Height);
-            
+
                 if (block.Height == 0) {
                     //we set settings too
                     ChainSettings settings = GetChainSettings();
@@ -73,7 +74,7 @@ namespace TangleChainIXI {
                 }
             }
 
-            
+
 
             return flag;
 
@@ -92,10 +93,14 @@ namespace TangleChainIXI {
             string sql = $"SELECT * FROM Block WHERE Height={height}";
             SQLiteDataReader reader = QuerySQL(sql);
 
-            if (!reader.Read())
+            if (!reader.Read()) {
+                reader.Close(); reader.Dispose();
                 return null;
+            }
 
             Block block = new Block(reader, CoinName);
+
+            reader.Close(); reader.Dispose();
 
             return block;
 
@@ -109,8 +114,10 @@ namespace TangleChainIXI {
 
             SQLiteDataReader reader = QuerySQL(sql);
 
-            if (!reader.Read())
+            if (!reader.Read()) {
+                reader.Close(); reader.Dispose();
                 return null;
+            }
 
             long ID = (long)reader[0];
             var output = GetTransactionOutput(ID);
@@ -119,7 +126,7 @@ namespace TangleChainIXI {
 
             trans.SendTo = Utils.GetTransactionPoolAddress(height, CoinName);
 
-            reader.Close();
+            reader.Close(); reader.Dispose();
             reader.Dispose();
 
             return trans;
@@ -141,6 +148,8 @@ namespace TangleChainIXI {
 
             long TransID = (long)reader[0];
 
+            reader.Close(); reader.Dispose();
+
             //add data too
             for (int i = 0; i < trans.Data.Count; i++) {
 
@@ -156,6 +165,7 @@ namespace TangleChainIXI {
 
                 NoQuerySQL(sql2);
             }
+
         }
 
         private List<string> GetTransactionData(long id) {
@@ -172,8 +182,10 @@ namespace TangleChainIXI {
 
             SQLiteDataReader reader = command.ExecuteReader();
 
-            if (!reader.Read())
+            if (!reader.Read()) {
+                reader.Close(); reader.Dispose();
                 return null;
+            }
 
             while (true) {
 
@@ -182,6 +194,8 @@ namespace TangleChainIXI {
                 if (!reader.Read())
                     break;
             }
+
+            reader.Close(); reader.Dispose();
 
             return list;
         }
@@ -212,6 +226,8 @@ namespace TangleChainIXI {
                     break;
             }
 
+            reader.Close(); reader.Dispose();
+
             return (listValue, listReceiver);
         }
 
@@ -220,11 +236,16 @@ namespace TangleChainIXI {
             //we first get highest ID
             SQLiteDataReader reader = QuerySQL($"SELECT MAX(Height) FROM Block");
 
-            if (reader.Read()) {
+            if (!reader.Read()) {
+                reader.Close(); reader.Dispose();
                 return null;
             }
 
-            return GetBlock((long)reader[0]);
+            long num = (long)reader[0];
+
+            reader.Close(); reader.Dispose();
+
+            return GetBlock(num);
 
         }
 
@@ -238,6 +259,8 @@ namespace TangleChainIXI {
 
             SQLiteDataReader reader = QuerySQL(sql);
             ChainSettings settings = new ChainSettings(reader);
+
+            reader.Close(); reader.Dispose();
 
             return settings;
 
@@ -270,7 +293,11 @@ namespace TangleChainIXI {
             SQLiteDataReader reader = QuerySQL(transFees);
             reader.Read();
 
-            return (long)reader[0] * -1;
+            long num = (long)reader[0] * -1;
+
+            reader.Close(); reader.Dispose();
+
+            return num;
         }
 
         private long GetOutgoingOutputs(string user) {
@@ -278,8 +305,11 @@ namespace TangleChainIXI {
             SQLiteDataReader reader = QuerySQL(sql_Outgoing);
             reader.Read();
 
-            return (long)reader[0] * -1;
+            long num = (long)reader[0] * -1;
 
+            reader.Close(); reader.Dispose();
+
+            return num;
         }
 
         private long GetIncomingOutputs(string user) {
@@ -287,7 +317,11 @@ namespace TangleChainIXI {
             SQLiteDataReader reader = QuerySQL(sql_Outputs);
             reader.Read();
 
-            return (long)reader[0];
+            long num = (long)reader[0];
+
+            reader.Close(); reader.Dispose();
+
+            return num;
         }
 
         private long GetBlockReward(string user) {
@@ -300,15 +334,20 @@ namespace TangleChainIXI {
             reader.Read();
 
             long blockReward = (long)reader[0] * Settings.ChainSettings[CoinName].BlockReward;
+            reader.Close(); reader.Dispose();
 
             ////you also get transaction fees
             string sql_TransFees = string.Format("SELECT IFNULL(SUM(Data),0) FROM Block AS b JOIN Transactions AS t ON " +
                                                 "b.Height = t.BlockID JOIN Data as d ON t.ID = d.TransID " +
                                                 "WHERE Owner = '{0}' AND _ArrayIndex = 0;", user);
+
+
             reader = QuerySQL(sql_TransFees);
             reader.Read();
 
             long TransFees = (long)reader[0];
+
+            reader.Close(); reader.Dispose();
 
             return blockReward + TransFees;
         }
@@ -318,18 +357,30 @@ namespace TangleChainIXI {
         #region SQL Utils
 
         public SQLiteDataReader QuerySQL(string sql) {
+
+
+            Db = new SQLiteConnection($@"Data Source={Settings.StorePath}{CoinName}\chain.db; Version=3;");
+            Db.Open();
+
             SQLiteCommand command = new SQLiteCommand(Db);
             command.CommandText = sql;
-            SQLiteDataReader reader = command.ExecuteReader();
+
+            SQLiteDataReader reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
             return reader;
         }
 
         public void NoQuerySQL(string sql) {
+
+            Db = new SQLiteConnection($@"Data Source={Settings.StorePath}{CoinName}\chain.db; Version=3;");
+            Db.Open();
+
             SQLiteCommand command = new SQLiteCommand(Db);
             command.CommandText = sql;
             command.ExecuteNonQuery();
 
             command.Dispose();
+            Db.Close();
         }
 
         #endregion
